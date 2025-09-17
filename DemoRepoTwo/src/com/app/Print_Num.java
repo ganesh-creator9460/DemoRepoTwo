370a5ec0a8f147dde89aac36d93077d58dec8740
ganesh-creator9460 <144310015+ganesh-creator9460@users.noreply.github.com>
2025-09-17 15:04:58 +0530

    Import s

--- DemoRepoTwo/src/com/app/Print_Num.java
+++ DemoRepoTwo/src/com/app/Print_Num.java
@@ -1,10 +1,14 @@
 package com.app;
-
+import java.util.Scanner;
 public class Print_Num {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num : ");
+		int num = sc.nextInt();
 		
-		for(int i=0;i<=100;i++)
+		for(int i=0;i<=num;i++)
 		{
 			System.out.println(i);
 		}
